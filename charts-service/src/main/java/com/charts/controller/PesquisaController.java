8be4d0c7dc0bc51641431803d1801e154078686f
miguel castro <miguel.castro9@hotmail.com>
2022-12-03 18:08:01 -0300

    config cors

--- charts-service/src/main/java/com/charts/controller/PesquisaController.java
+++ charts-service/src/main/java/com/charts/controller/PesquisaController.java
@@ -5,6 +5,7 @@ import com.charts.service.PesquisaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/pesquisa")
+@CrossOrigin(origins = "*")
 public class PesquisaController {
 
     @Autowired
